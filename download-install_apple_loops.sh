077dbfc3fea91a5ab2a534ae7b432227c7bcad87
Shannon Pasto <shannonpasto@me.com>
2025-01-18 11:19:34 +1100

    1.2
    is paramater 4 is not set, search /Applications for any/all plists

--- download-install_apple_loops.sh
+++ download-install_apple_loops.sh
@@ -4,7 +4,7 @@
 # download-install_apple_loops.sh - script to download and install all available Apple loops for the specified plist
 # Shannon Pasto https://github.com/shannonpasto/AppleLoops
 #
-# v1.1.1 (18/01/2025)
+# v1.2 (18/01/2025)
 ###################
 
 ## uncomment the next line to output debugging to stdout
@@ -16,8 +16,7 @@
 ME=$(basename "$0")
 # shellcheck disable=SC2034
 BINPATH=$(dirname "$0")
-appPlist=""  # garageband1047 logicpro1110 mainstage362
-jqBin=$(whereis -qb jq)
+appPlist=""  # garageband1047 logicpro1110 mainstage362. multiple plists can be specified, separate with a space
 
 ###############################################################################
 ## function declarations
@@ -25,7 +24,7 @@ jqBin=$(whereis -qb jq)
 exit_trap() {
 
   # clean up
-  /bin/rm -rf "${tmpDir}"
+  /bin/rm -rf "${tmpDir}" >/dev/null 2>&1
   /bin/kill "${cafPID}" >/dev/null 2>&1
 
 }
@@ -40,9 +39,6 @@ if [ "${4}" != "" ] && [ "${appPlist}" = "" ]; then
   appPlist="${4}"
 elif [ "${4}" != "" ] || [ "${appPlist}" != "" ]; then
   /bin/echo "Parameter 4 overwritten by script variable"
-elif [ "${4}" = "" ] && [ "${appPlist}" = "" ]; then
-  /bin/echo "Required parameter 4 not set. Exiting"
-  exit 1
 fi
 
 tmpDir="/tmp/${appPlist}"
@@ -50,7 +46,7 @@ tmpDir="/tmp/${appPlist}"
 
 # see if we have a caching server on the network. pick the first one
 if [ "$(/usr/bin/sw_vers -buildVersion | /usr/bin/cut -c 1-2 -)" -ge 24 ]; then
-  cacheSrvrURL=$(/usr/bin/AssetCacheLocatorUtil -j 2>/dev/null | "${jqBin}" -r '.results.reachability[]' | /usr/bin/head -n 1)
+  cacheSrvrURL=$(/usr/bin/AssetCacheLocatorUtil -j 2>/dev/null | /usr/bin/jq -r '.results.reachability[]' | /usr/bin/head -n 1)
 else
   cacheSrvrURL=$(/usr/bin/AssetCacheLocatorUtil -j 2>/dev/null | /usr/bin/plutil -extract results.reachability.0 raw -o - -)
 fi
@@ -73,25 +69,42 @@ fi
 /usr/bin/caffeinate -ims &
 cafPID=$(pgrep caffeinate)
 
-# get the plist file
-/bin/echo "getting the main plist for ${appPlist}"
-/usr/bin/curl -s "${baseURL}/${appPlist}.plist${baseURLOpt}" -o "${tmpDir}/${appPlist}".plist
+if [ "${appPlist}" = "" ]; then
+  /bin/echo "No plist configured as a parameter. Searching /Applications for any/all apps"
+  plistNames="garageband logicpro mainstage"
+  for X in $plistNames; do
+    /usr/bin/find /Applications -name "${X}*.plist" | /usr/bin/rev | /usr/bin/cut -d "/" -f 1 - | /usr/bin/rev | /usr/bin/cut -d "." -f 1 - >> "${tmpDir}/thelist.txt"
+  done
 
-if ! /usr/bin/plutil "${tmpDir}/${appPlist}".plist >/dev/null 2>&1; then
-  /bin/echo "Invalid plist file. Exiting"
-  exit 1
+  if [ ! -s "${tmpDir}/thelist.txt" ]; then
+    /bin/echo "No valid application found. Exiting"
+    exit 1
+  else
+    appPlist=$(/bin/cat "${tmpDir}/thelist.txt")
+  fi
 fi
 
-# loop through all the pkg files and download/install
-for thePKG in $(/usr/bin/defaults read "${tmpDir}/${appPlist}".plist Packages | /usr/bin/grep DownloadName | /usr/bin/awk -F \" '{print $2}'); do
-  thePKGFile=$(/bin/echo "${thePKG}" | /usr/bin/sed 's/..\/lp10_ms3_content_2013\///')
-  if ! /usr/sbin/pkgutil --pkgs | /usr/bin/grep "$(basename "${thePKGFile}" .pkg)"; then
-    /bin/echo "Installing ${thePKGFile}"
-    /usr/bin/curl -s "${baseURL}/${thePKG}${baseURLOpt}" -o "${tmpDir}/${thePKGFile}"
-    /usr/sbin/installer -pkg "${tmpDir}/${thePKGFile}" -target /
-  else
-    /bin/echo "${thePKGFile} already installed"
+for X in $appPlist; do
+  # get the plist file
+  /bin/echo "Fetching the Apple plist for ${X}"
+  /usr/bin/curl -s "${baseURL}/${X}.plist${baseURLOpt}" -o "${tmpDir}/${X}".plist
+
+  if ! /usr/bin/plutil "${tmpDir}/${X}".plist >/dev/null 2>&1; then
+    /bin/echo "Invalid plist file. Exiting"
+    exit 1
   fi
+
+  # loop through all the pkg files and download/install
+  for thePKG in $(/usr/bin/defaults read "${tmpDir}/${X}".plist Packages | /usr/bin/grep DownloadName | /usr/bin/awk -F \" '{print $2}'); do
+    thePKGFile=$(/bin/echo "${thePKG}" | /usr/bin/sed 's/..\/lp10_ms3_content_2013\///')
+    if ! /usr/sbin/pkgutil --pkgs | /usr/bin/grep "$(basename "${thePKGFile}" .pkg)"; then
+      /bin/echo "Installing ${thePKGFile}"
+      /usr/bin/curl -s "${baseURL}/${thePKG}${baseURLOpt}" -o "${tmpDir}/${thePKGFile}"
+      /usr/sbin/installer -pkg "${tmpDir}/${thePKGFile}" -target /
+    else
+      /bin/echo "${thePKGFile} already installed"
+    fi
+  done
 done
 
 exit 0
